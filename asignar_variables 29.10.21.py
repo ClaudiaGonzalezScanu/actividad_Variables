2313562e63399ae208432f9f611ac9eee49be9cb
Claudia González <claudia.gonzalezscanu@gmail.com>
2021-11-01 19:04:17 -0300

    subiendo codigo

---
+++ asignar_variables 29.10.21.py
@@ -0,0 +1,12 @@
+#esto es un comentario en python y a continuación declarare variables
+var = "5"
+var_1 = "99"
+var_2 = True
+#estoy aprendiendo a comparar variables
+print("el valor de var es")
+print(type(var))
+print("el valor de var_1 es")
+print(type(var_1))
+print(type(var_2))
+
+
---
+++ hola_mundo.py
@@ -0,0 +1,17 @@
+#ejercicio 29.10.21
+print("Hola Mundo")
+print("Claudia")
+print("Soy una persona que vive en el planeta")
+print("Dime tu nombre")
+#esto es un comentario en python y a continuación declarare variables
+var = "5"
+var_1 = "99"
+var_2 = True
+#estoy aprendiendo a comparar variables
+print("el valor de var es")
+print(type(var))
+print("el valor de var_1 es")
+print(type(var_1))
+print(type(var_2))
+
+
